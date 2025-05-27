619a5783f0f6ac7ed06467693b97bdbc79250c2e
Kai Hendry <hendry@iki.fi>
2025-05-27 12:13:51 +0100

    simplify

--- bucket_policy.tf
+++ bucket_policy.tf
@@ -6,31 +6,23 @@ resource "aws_s3_bucket_policy" "secure_bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "DenyAllOtherAccess",
+        Sid       = "RestrictedDataAccess",
         Effect    = "Deny",
         Principal = "*",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ],
+        Action    = "s3:*",
         Resource = [
           aws_s3_bucket.secure_bucket.arn,
           "${aws_s3_bucket.secure_bucket.arn}/*"
         ],
         Condition = {
-          StringNotLike = {
+          StringNotEquals = {
             "aws:PrincipalArn" = [
               "arn:aws:iam::407461997746:user/hendry",
               "arn:aws:iam::407461997746:role/github-actions-Role-56IHHM969DKJ",
-              "arn:aws:iam::407461997746:role/AWSReservedSSO_AdministratorAccess_*",
-              "arn:aws:sts::407461997746:assumed-role/AWSReservedSSO_AdministratorAccess_*/*"
+              "arn:aws:iam::407461997746:role/AWSReservedSSO_AdministratorAccess_faa8fd51f242b1ab",
             ]
           },
-          Null = {
-            "s3:DataAccessPointArn" = "true"
-          }
+          "s3:DataAccessPointAccount" = data.aws_caller_identity.current.account_id
         }
       }
     ]
