0a315e97e259f9604d92921b7f92731fe914ed25
Kai Hendry <hendry@iki.fi>
2025-05-27 13:12:50 +0100

    another attempt to fix policy

--- bucket_policy.tf
+++ bucket_policy.tf
@@ -20,9 +20,10 @@ resource "aws_s3_bucket_policy" "secure_bucket_policy" {
               "arn:aws:iam::407461997746:user/hendry",
               "arn:aws:iam::407461997746:role/github-actions-Role-56IHHM969DKJ",
               "arn:aws:iam::407461997746:role/AWSReservedSSO_AdministratorAccess_faa8fd51f242b1ab",
-            ],
-            "s3:DataAccessPointAccount" = data.aws_caller_identity.current.account_id
-          }
+              "arn:aws:iam::407461997746:role/aws-reserved/sso.amazonaws.com/ap-southeast-1/AWSReservedSSO_AdministratorAccess_faa8fd51f242b1ab"
+            ]
+          },
+          "s3:DataAccessPointAccount" = data.aws_caller_identity.current.account_id
         }
       }
     ]
