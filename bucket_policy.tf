0585baf03531b0fcc99b225c1b9f734eab97db29
Kai Hendry <hendry@iki.fi>
2025-05-27 12:47:50 +0100

    address https://github.com/kaihendry/s3-check-role/actions/runs/15273781973/job/42955204419

--- bucket_policy.tf
+++ bucket_policy.tf
@@ -20,9 +20,9 @@ resource "aws_s3_bucket_policy" "secure_bucket_policy" {
               "arn:aws:iam::407461997746:user/hendry",
               "arn:aws:iam::407461997746:role/github-actions-Role-56IHHM969DKJ",
               "arn:aws:iam::407461997746:role/AWSReservedSSO_AdministratorAccess_faa8fd51f242b1ab",
-            ]
-          },
-          "s3:DataAccessPointAccount" = data.aws_caller_identity.current.account_id
+            ],
+            "s3:DataAccessPointAccount" = data.aws_caller_identity.current.account_id
+          }
         }
       }
     ]
